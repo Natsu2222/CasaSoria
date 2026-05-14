e4c29a6e60d2ed0ed17b4eb11c1f52ad9d3522f1
Sam Hatami <samhatami@MacBook-Pro-di-Sam.local>
2026-05-14 14:22:14 +0200

    migrations

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260514_121847 from './20260514_121847';
+
+export const migrations = [
+  {
+    up: migration_20260514_121847.up,
+    down: migration_20260514_121847.down,
+    name: '20260514_121847'
+  },
+];
